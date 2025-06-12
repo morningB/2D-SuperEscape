fb01970579ae5b8a62cae2103a9312ed211638cc
Joowon_Yun <yjw001205@gmail.com>
2025-06-12 23:48:15 +0900

    feat : 적 체력 기능

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public Rigidbody2D rb;
-
+    public int enemyHealth = 3;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -12,9 +12,18 @@ public class Enemy : MonoBehaviour
 
     public void TakeDamage(int damage, Vector2 knockback)
     {
+        if (enemyHealth <= 0)
+        {
+            //GetComponent<CapsuleCollider2D>().enabled = false;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+            GetComponent<Animator>().enabled = false;
+            GetComponent<EnemyPatrol>().enabled = false;
+            Destroy(gameObject, 1f);
+        }
         rb.linearVelocity = Vector2.zero;  // 기존 속도 제거
         rb.AddForce(knockback, ForceMode2D.Impulse);
         // 체력 감소, 피격 애니메이션, 무적 처리 등 추가 가능
+        enemyHealth -= damage;
     }
 }
 
