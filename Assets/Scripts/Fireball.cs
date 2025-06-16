4777ad622015b17e339dc873b1ae4aecfa0d3d02
morningB <yjw001205@gmail.com>
2025-06-16 17:15:22 +0900

    fix : 플레이어 충돌 시 삭제

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 public class Fireball : MonoBehaviour
 {
+    public GameObject FireballExplosionPrefab;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             // 데미지 처리, 이펙트 등
+            GameObject explosion = Instantiate(FireballExplosionPrefab, transform.position, Quaternion.identity);
+            Destroy(explosion, 0.5f);  
             Destroy(gameObject);
         }
         else if (other.CompareTag("Wall"))
