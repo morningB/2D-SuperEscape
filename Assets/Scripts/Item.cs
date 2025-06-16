87399d885483cb17d3c1d1c378fd602d9116e11d
Joowon_Yun <yjw001205@gmail.com>
2025-06-16 21:02:50 +0900

    feat: 아이템 획득 시 체력 증가

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,12 +5,17 @@ public class Item : MonoBehaviour
 {
     public Text itemGetText;
     int count = 0;
+    public AudioClip getItemAudio;
+    public PlayerHealth health;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Treasure"))
         {
             Debug.Log("아이템획득");
             count++;
+            if(health.currentHealth < health.playerMaxHealth)
+                health.currentHealth++;
+            GetComponent<AudioSource>().PlayOneShot(getItemAudio);
             itemGetText.text = count + " / 6";
             Destroy(collision.gameObject);
         }
